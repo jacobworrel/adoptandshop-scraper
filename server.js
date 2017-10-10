29d4daf54fadea13de0b760b8d97f19f310cb9cb
jacobworrel <jacobworrel@gmail.com>
2017-10-09 23:20:36 -0700

    add procfile

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const app = express();
 const path = require('path');
 const scraper = require('./scraper');
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(path.join(__dirname, './node_modules/')));
 app.use(express.static(path.join(__dirname, './client/')));
 
 app.get('/culvercity', scraper.getData);
 app.get('/lakewood', scraper.getData);
 
-app.listen(3000);
+app.listen(app.get('port'));
 
 module.exports = app;
