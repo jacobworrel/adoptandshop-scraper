23eaef5376ec2c3140d5b8ade786576e55d47f48
jacobworrel <jacobworrel@gmail.com>
2017-10-12 00:14:49 -0700

    add cat gif, change color palette

--- client/controllers/homeController.js
+++ client/controllers/homeController.js
@@ -12,13 +12,14 @@ function HomeController($scope, $http, $apply) {
   $scope.animals = [];
   $scope.search;
   $scope.sortBy;
+
   const promises = urls.map(url => $http.get(url).catch(() => console.log(err)));
   Promise.all(promises)
     .then(res => {
       $scope.animals = res.reduce((a, c) => [...a, ...c.data], []);
       $scope.$apply();
-      const el = document.getElementsByTagName('html')[0];
-      el.className = el.className.replace(/loading/, ' ');
+      const el = document.getElementsByClassName('loading')[0];
+      el.style.display = 'none';
     })
     .catch(err => console.log(err));
 }
